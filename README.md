9a9635e67c6edeb00bd85d58ea857e959108f7d3
swathysroy <swathysroy@gmail.com>
2021-02-17 17:21:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-374tgg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-374tgg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  counter=0;
+
+  @HostListener('window:keydown',['$event'])
+  handleKeyDown(event:KeyboardEvent){
+    this.counter++;
+  }
+
+  resetCounter(){
+    this.counter=0;
+  }
+}
